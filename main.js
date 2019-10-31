301a1345f0bd25b04140076b866b12d20b0abd35
rakeshkr114 <M1039158@mindtree.com>
2019-10-31 12:43:01 +0530

    image insertion

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'socks',
-        description: 'Brand new socks!!!'
+        description: 'Brand new socks!!!',
+        image: './assets/socks.jpg'
     }
 })
